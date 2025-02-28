8529b2659048756a22b8eded22b0a2be8b5d29b2
gomminjae <gommj0611@naver.com>
2025-02-28 20:14:34 +0900

    Feat: Profile

---
+++ Newdok/Source/Feature/Article/Alert/EmailInfoView.swift
@@ -0,0 +1,24 @@
+//
+//  EmailInfoView.swift
+//  Newdok
+//
+//  Created by 권민재 on 2/28/25.
+//
+
+import SwiftUI
+
+struct EmailInfoView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                
+            }
+        }
+        .background(Color("#25242C99"))
+    }
+}
+
+#Preview {
+    EmailInfoView()
+}
+
