d2697af58eabf8cf1bfaf7d054605ad8994d71a7
gomminjae <gommj0611@naver.com>
2025-03-03 14:32:42 +0900

    Feat: Bookmark

---
+++ Newdok/Source/Feature/BookMark/BookMarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  BookMarkRow.swift
+//  Newdok
+//
+//  Created by 권민재 on 3/2/25.
+//
+
+import SwiftUI
+
+struct BookMarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookMarkRow()
+}
