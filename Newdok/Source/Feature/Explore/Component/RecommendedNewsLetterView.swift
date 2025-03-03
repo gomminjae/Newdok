ba2f544cd8d28e37de73de552fa83dfc77a31f4e
gomminjae <gommj0611@naver.com>
2025-03-03 14:32:42 +0900

    Feat: Bookmark

---
+++ Newdok/Source/Feature/Explore/Component/RecommendedNewsLetterView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsLetterView.swift
+//  Newdok
+//
+//  Created by 권민재 on 3/2/25.
+//
+
+import SwiftUI
+
+struct RecommendedNewsLetterView: View {
+    var body: some View {
+        VStack {
+            Image("signup")
+        }
+    }
+}
+
+#Preview {
+    NewsLetterView()
+}
